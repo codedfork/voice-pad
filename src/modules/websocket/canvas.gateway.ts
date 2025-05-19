cc531c9cb4cbcf7383f4f02f934b069e3244e9f5
codedfork <ksourav8920@gmail.com>
2025-05-19 15:49:47 +0530

    Auto-generated commit message

--- src/modules/websocket/canvas.gateway.ts
+++ src/modules/websocket/canvas.gateway.ts
@@ -1,17 +1,30 @@
-// canvas.gateway.ts
 import {
     SubscribeMessage,
     WebSocketGateway,
     WebSocketServer,
     MessageBody,
     ConnectedSocket,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class CanvasGateway {
+export class CanvasGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
     server: Server;
+
+    private connectedUsers: string[] = [];
+
+    handleConnection(client: Socket) {
+        this.connectedUsers.push(client.id);
+        this.server.emit('participants', this.connectedUsers);
+    }
+
+    handleDisconnect(client: Socket) {
+        this.connectedUsers = this.connectedUsers.filter(id => id !== client.id);
+        this.server.emit('participants', this.connectedUsers);
+    }
 
     @SubscribeMessage('draw')
     handleDraw(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
@@ -20,11 +33,21 @@ export class CanvasGateway {
 
     @SubscribeMessage('signal')
     handleSignal(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
-        client.broadcast.emit('signal', data);
+        client.broadcast.emit('signal', { ...data, from: client.id });
     }
 
     @SubscribeMessage('subtitle')
     handleSubtitle(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
         client.broadcast.emit('subtitle', data);
+    }
+
+    @SubscribeMessage('clear')
+    handleClear(@ConnectedSocket() client: Socket) {
+        client.broadcast.emit('clear');
+    }
+
+    @SubscribeMessage('ready')
+    handleReady(@ConnectedSocket() client: Socket) {
+        client.broadcast.emit('new-user', client.id);
     }
 }
